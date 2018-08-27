055c00399cb1ef575be774a28d2ad32bab453b0f
Imunity <42749600+Imunity@users.noreply.github.com>
2018-08-27 16:35:38 -0300

    Add files via upload

---
+++ Cgui fc.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+	int idade;
+	int a,b,menor;
+	/*p
+	printf("%s esta a %d milhoes de milhas do sol","Venus",67);
+	printf("Valor inteiro atribuido foi %d para o caracter %c e um float foi de %f", 99, 'a', 1.45);
+	printf("Se quisesse imprimir uma string: %s","Minha string!"); 
+	printf("\a");
+	printf("\n \n \n");
+	printf(" \t \t \t \a test");
+	printf("default: %f \n",3.1415169265);
+	printf("uma casa: %.1f \n",3.1415169265);
+	printf("duas casas: %.2f \n",3.1415169265);
+	printf("tres casas: %.3f \n",3.1415169265);
+	printf("notaçao cientifica: %e \n",3.1415169265);
+	printf("Belem \n");
+	printf("Meu nome e %s tenho %d anos e %.2f de altura \n", "guilherme", 18, 1.75);
+	printf("%.5f \n", 75.7632489);
+	printf("%.1f \n", 75.7632489);
+	printf("%.2f",75.7632489);
+	printf("digite sua idade: ");
+	scanf("%d", &idade);
+	printf("a sua idade e %d",idade);
+	return 0;
+	printf("digite dois numeros inteiros:");
+	scanf("%d %d",&a,&b);
+	menor=a;
+	if (a>b)
+	menor=b;
+	printf("o menor numero digitado foi %d:", menor);
+	return 0;
+	*/
+}
